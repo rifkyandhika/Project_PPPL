9ec5e1985cf4301c9d4eb1ec2cd38ebd182fa7ec
MaulanaAzis <azissptr17@gmail.com>
2021-10-29 22:49:55 +0700

    uji coba git

--- application/views/2.php
+++ application/views/2.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h1>hello word</h1>
+<body> 
+    Hello World
 </body>
 </html>
