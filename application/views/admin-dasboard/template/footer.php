029169c4c4823527e117f7a455c0bb090efee877
nurianc <nurhandrianc4@gmail.com>
2021-12-23 19:21:14 +0700

    video 18

---
+++ application/views/admin-dasboard/template/footer.php
@@ -0,0 +1,21 @@
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+        <script src="<?= base_url('asset')?>/plugins/popper.js/dist/umd/popper.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/screenfull/dist/screenfull.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+        <script src="<?= base_url('asset')?>/dist/js/theme.min.js"></script>
+        <script src="<?= base_url('asset')?>/js/datatables.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
