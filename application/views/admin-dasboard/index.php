2c6b94032b170120235715ec167c21c43c411f2d
nurianc <nurhandrianc4@gmail.com>
2021-12-23 18:49:41 +0700

    video 18

--- application/views/admin-dasboard/index.php
+++ application/views/admin-dasboard/index.php
@@ -1,4 +1,4 @@
-
+                
                 <div class="main-content">
                     <div class="container-fluid">
                         <div class="page-header">
@@ -16,7 +16,7 @@
                                     <nav class="breadcrumb-container" aria-label="breadcrumb">
                                         <ol class="breadcrumb">
                                             <li class="breadcrumb-item">
-                                                <a href="../index.html"><i class="ik ik-home"></i></a>
+                                                <a href="</index.html"><i class="ik ik-home"></i></a>
                                             </li>
                                             <li class="breadcrumb-item">
                                                 <a href="#">Tables</a>
@@ -1874,3 +1874,25 @@
                         </div>
                     </div>
                 </div>
+
+                <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+        <script src="<?= base_url('asset')?>/plugins/popper.js/dist/umd/popper.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/screenfull/dist/screenfull.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="<?= base_url('asset')?>/plugins/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+        <script src="<?= base_url('asset')?>/dist/js/theme.min.js"></script>
+        <script src="<?= base_url('asset')?>/js/datatables.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
